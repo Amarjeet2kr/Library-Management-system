307233a72f54173f48e884f034e01bf42e3469b4
Amarjeet Kumar <amarjeeth228@gmail.com>
2021-04-12 11:41:27 +0530

    login page has been dont

--- src/LibrarySystem.cpp
+++ src/LibrarySystem.cpp
@@ -4,17 +4,46 @@
 
 //*******************************************************************************************************/
 
-
 #include "LibrarySystem.h"
-
 using namespace std;
+
+string username[]={"amar","vivek"};
+string password[]={"pass","pass"};
+int loginFlag=false;
+
+void adminLogin()
+{
+    string userNameInput, passInput;
+    while (loginFlag==false)
+    {
+        system("cls");
+        cout<<"\n\n\n\n";
+        cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+        cout<<">>\t\t\t\tLIBRARY MANAGEMENT SYSTEM\t\t\t\t     >>"<<endl;
+        cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl<<endl;
+        cout<<"\t\t\t\tLibrary System Login"<<endl<<endl;
+        cout<<"\t\t\t\tEnter username: ";
+        cin>>userNameInput;
+        cout<<"\t\t\t\tEnter password: ";
+        cin>>passInput;
+        for (int i = 0; i <= 2; i++)
+        {
+            if(userNameInput==username[i] && passInput==password[i])
+            {
+                loginFlag=1;
+            }
+        }
+        
+    }
+    
+}
 
 int main()
 {
-    system("cls");
-    cout<<"LIBRARY MANAGEMENT SYSTEM"<<endl;
-
-
-
+    adminLogin();
+    if(loginFlag)
+    {
+        cout<<"welcome";
+    }
     return 0;
 }
